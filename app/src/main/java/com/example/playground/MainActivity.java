82eac7ab2023b1eb7b0f58b9d59b52f12f1771a0
summer <skyjimzhoujie@gmail.com>
2019-11-12 21:00:01 +0800

    rxjavatest

--- app/src/main/java/com/example/playground/MainActivity.java
+++ app/src/main/java/com/example/playground/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
@@ -18,10 +19,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.internal.operators.flowable.FlowableFromArray;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.schedulers.TestScheduler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,5 +63,47 @@ public class MainActivity extends AppCompatActivity {
      */
     public void btExchangeCard(View view) {
         startActivity(new Intent(MainActivity.this, ExchangeCardActivity.class));
+    }
+
+    /**
+     * Rxjava测试
+     * @param view
+     */
+    public void testRxJava(View view) {
+        int maxnumber = 10000;
+        String[] strings = new String[maxnumber];
+        for (int i = 0; i < maxnumber; i++) {
+            strings[i] = "" + i;
+        }
+        FlowableFromArray flowableFromArray = new FlowableFromArray(strings);
+        final Disposable subscribe = flowableFromArray
+                .subscribeOn(Schedulers.computation())
+                .subscribe(new Consumer() {
+            @Override
+            public void accept(Object o) throws Throwable {
+                Log.d("=summerzhou=", "(MainActivity.accept): o=" + o);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Throwable {
+                Log.d("=summerzhou=", "(MainActivity.Error): msg=" + throwable.getLocalizedMessage());
+            }
+        }, new Action() {
+            @Override
+            public void run() throws Throwable {
+                Log.d("=summerzhou=","(MainActivity.complete): ");
+            }
+        });
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("=summerzhou=","(MainActivity.dispose): ");
+                subscribe.dispose();
+            }
+        }, 300);
+
     }
 }
