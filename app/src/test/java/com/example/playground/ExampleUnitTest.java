25cfd1c747713720adf824674baeff22b6e0ff84
zhoujie <summer.zhou@webeye.com>
2022-02-16 17:22:42 +0800

    5

--- app/src/test/java/com/example/playground/ExampleUnitTest.java
+++ app/src/test/java/com/example/playground/ExampleUnitTest.java
@@ -362,4 +362,22 @@ public class ExampleUnitTest {
         int abcabcbb = lengthOfLongestSubstring("pwwkew");
         Assert.assertEquals(3, abcabcbb);
     }
+
+    /**
+     * 
+     * 5. 最长回文子串
+     * 给你一个字符串 s，找到 s 中最长的回文子串。
+     *
+     * 示例 1：
+     *
+     * 输入：s = "babad"
+     * 输出："bab"
+     * 解释："aba" 同样是符合题意的答案。
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+
+    }
 }
