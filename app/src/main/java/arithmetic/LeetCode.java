9b535e4d01cca70a3f29a878edd65e465314ba55
summer <skyjimzhoujie@gmail.com>
2024-02-21 17:57:41 +0800

    leetcode

--- app/src/main/java/arithmetic/LeetCode.java
+++ app/src/main/java/arithmetic/LeetCode.java
@@ -11,6 +11,57 @@ public class LeetCode {
 
     }
 
+    /**
+     * 106. 从中序与后序遍历序列构造二叉树
+     * 给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历， postorder 是同一棵树的后序遍历，请你构造并返回这颗 二叉树 。
+     */
+    class BuildTree_106 {
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public TreeNode buildTree_106(int[] inorder, int[] postorder) {
+            if (inorder == null || inorder.length == 0) {
+                return null;
+            }
+            HashMap<Integer, Integer> valueIndexInInorderMap = new HashMap<>();
+            for (int i = 0; i < inorder.length; i++) {
+                valueIndexInInorderMap.put(inorder[i], i);
+            }
+            return buildTreeNodeRecursive(valueIndexInInorderMap,inorder,postorder,0,inorder.length-1,0,postorder.length-1);
+        }
+
+        public TreeNode buildTreeNodeRecursive(HashMap<Integer, Integer> valueIndexInInorderMap, int[] inorder, int[] postorder, int inorderLeft, int inorderRight, int postorderLeft, int postorderRight) {
+            if (inorderLeft > inorderRight || postorderLeft > postorderRight) {
+                return null;
+            }
+            TreeNode root = new TreeNode(postorder[postorderRight]);
+            int indexOfRootInInorder = valueIndexInInorderMap.get(root.val);
+            int leftTreeLength = indexOfRootInInorder - inorderLeft;
+            root.left = buildTreeNodeRecursive(valueIndexInInorderMap, inorder, postorder, inorderLeft, indexOfRootInInorder - 1, postorderLeft, postorderLeft + leftTreeLength - 1);
+            root.right = buildTreeNodeRecursive(valueIndexInInorderMap, inorder, postorder, indexOfRootInInorder+1, inorderRight, postorderLeft + leftTreeLength, postorderRight-1);
+            return root;
+        }
+
+        public TreeNode buildTree_106_PlanB(int[] inorder, int[] postorder) {
+            if (inorder == null || inorder.length == 0) {
+                return null;
+            }
+            HashMap<Integer, Integer> valueIndexInInorderMap = new HashMap<>();
+            for (int i = 0; i < inorder.length; i++) {
+                valueIndexInInorderMap.put(inorder[i], i);
+            }
+            return buildTreeNodeRecursive(valueIndexInInorderMap,inorder,postorder,0,inorder.length-1,0,postorder.length-1);
+        }
+    }
 
     /**
      * 105. 从前序与中序遍历序列构造二叉树
