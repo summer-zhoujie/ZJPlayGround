7b097e843e05531132e13a57313037a0581f996b
summer <skyjimzhoujie@gmail.com>
2024-04-02 11:04:33 +0800

    adaptive icon

--- app/src/main/java/arithmetic/LeetCode.java
+++ app/src/main/java/arithmetic/LeetCode.java
@@ -58,6 +58,31 @@ public class LeetCode {
     }
 
     /**
+     * 894. 所有可能的真二叉树
+     */
+    static class Solution_894 {
+
+        //Definition for a binary tree node.
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+
+        public List<TreeNode> allPossibleFBT(int n) {
+            return null;
+        }
+    }
+
+    /**
      * 2810. 故障键盘
      */
     static class Solution_2810 {
