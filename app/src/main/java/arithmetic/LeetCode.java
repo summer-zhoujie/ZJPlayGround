1665d86a6ea0e4f45a45ca5d9a29942b7de9ad15
summer <skyjimzhoujie@gmail.com>
2024-05-28 09:43:45 +0800

    leetcode

--- app/src/main/java/arithmetic/LeetCode.java
+++ app/src/main/java/arithmetic/LeetCode.java
@@ -133,6 +133,21 @@ public class LeetCode {
     }
 
     /**
+     * 2951. 找出峰值
+     */
+    class Solution_2951 {
+        public List<Integer> findPeaks(int[] mountain) {
+            List<Integer> res = new ArrayList<>();
+            for (int i = 1; i < mountain.length-1; i++) {
+                if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+                    res.add(i);
+                }
+            }
+            return res;
+        }
+    }
+
+    /**
      * 2028. 找出缺失的观测数据
      */
     class Solution_2028 {
